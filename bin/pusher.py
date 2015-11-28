c670be688c01fc0ede9a438891af2726b64ca991
ziggy42 <vanpivix@gmail.com>
2015-11-28 22:02:22 +0100

    push file

--- bin/pusher.py
+++ bin/pusher.py
@@ -14,6 +14,7 @@ subparsers.required = True
 parser_push = subparsers.add_parser('push', help='push command')
 parser_push.add_argument('body', help='the main content of the push')
 parser_push.add_argument('title', help='the title of the push', nargs='?')
+parser_push.add_argument('-f', metavar='path', help='file')
 
 # List
 parser_list = subparsers.add_parser('list', help="list command")
@@ -25,11 +26,14 @@ parser_list = subparsers.add_parser('user', help="display user info")
 args = parser.parse_args()
 
 if args.cmd == 'push':
-    pusher_api.push(args.body, args.title)
+    if args.f:
+        pusher_api.push_file(args.f, args.body)
+    else:
+        pusher_api.push(args.body, args.title)
 elif args.cmd == 'list':
     if args.d:
         pusher_api.list_devices()
     else:
         pusher_api.pushes_list()
-elif args.user:
+elif args.cmd == 'user':
     pusher_api.user_info()
