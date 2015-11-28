3ffa124ac594bebd2209ae5dc9813ca1d8957a63
ziggy42 <vanpivix@gmail.com>
2015-11-28 22:16:55 +0100

    parser setup moved to a function

--- bin/pusher.py
+++ bin/pusher.py
@@ -4,26 +4,28 @@ import argparse
 from pusher import api
 
 
+def setup_parser():
+    parser = argparse.ArgumentParser(description='Pusher is a PushBullet command line client')
+    subparsers = parser.add_subparsers(dest='cmd')
+    subparsers.required = True
+
+    # Push
+    parser_push = subparsers.add_parser('push', help='push command')
+    parser_push.add_argument('body', help='the main content of the push')
+    parser_push.add_argument('title', help='the title of the push', nargs='?')
+    parser_push.add_argument('-f', metavar='path', help='file')
+
+    # List
+    parser_list = subparsers.add_parser('list', help="list command")
+    parser_list.add_argument('-d', action='store_true', help="list devices")
+
+    # User
+    parser_list = subparsers.add_parser('user', help="display user info")
+    return parser.parse_args()
+
+
 pusher_api = api.API()
-
-parser = argparse.ArgumentParser(description='Pusher is a PushBullet command line client')
-subparsers = parser.add_subparsers(dest='cmd')
-subparsers.required = True
-
-# Push
-parser_push = subparsers.add_parser('push', help='push command')
-parser_push.add_argument('body', help='the main content of the push')
-parser_push.add_argument('title', help='the title of the push', nargs='?')
-parser_push.add_argument('-f', metavar='path', help='file')
-
-# List
-parser_list = subparsers.add_parser('list', help="list command")
-parser_list.add_argument('-d', action='store_true', help="list devices")
-
-# User
-parser_list = subparsers.add_parser('user', help="display user info")
-
-args = parser.parse_args()
+args = setup_parser()
 
 if args.cmd == 'push':
     if args.f:
