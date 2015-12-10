68a4c030c2df3a0d3f92d4dc97a32354ca74f77b
ziggy42 <vanpivix@gmail.com>
2015-12-10 12:46:15 +0100

    SMS.py example

---
+++ examples/SMS.py
@@ -0,0 +1,13 @@
+from pusher import api
+
+data = {
+	"conversation_iden": "+39 123456789",
+    "message": "Hello!",
+    "package_name": "com.pushbullet.android",
+    "source_user_iden": "useriden",
+    "target_device_iden": "deviceiden",
+    "type": "messaging_extension_reply"
+}
+
+api = api.API()
+api.send_ephemeral(data)
